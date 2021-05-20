5874e760e4f0421e2bf421b2bafba7144b085392
Yanis Dreymanis <ydreiman@gmail.com>
2021-05-20 20:23:01 +0300

    Решение: Задание 1 - Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,4 @@
 function checkSpam(str) {
-  // ваш код...
+  return (str.toLowerCase().indexOf('1xBet'.toLowerCase()) !== -1 || 
+          str.toLowerCase().indexOf('XXX'.toLowerCase()) !== -1)
 }
