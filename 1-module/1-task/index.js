fe2791bcf8ec5d0ae4fb29ed77fe5050772b8362
Yanis Dreymanis <ydreiman@gmail.com>
2021-05-20 15:07:42 +0300

    Решение: Занятие 1 - Вычислить факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+
+  if (n===0 || n === 1) {
+    return result;
+  }
+
+  if (n > 1) {
+    for (let i = 2; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
 }
