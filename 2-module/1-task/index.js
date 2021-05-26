da85e5ea3fa8fefa0004d77ad5ac1cd81768fc2c
Yanis Dreymanis <ydreiman@gmail.com>
2021-05-26 11:28:47 +0300

    Занятие 2: правка 1 задачи

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,7 +1,7 @@
 function sumSalary(salaries) {
   let sum = 0
   for (let key in salaries) {
-    if (typeof salaries[key] === 'number' && isFinite(salaries[key]) === true){
+    if (typeof salaries[key] === 'number' && isFinite(salaries[key])){
       sum += salaries[key];
     }
   }
