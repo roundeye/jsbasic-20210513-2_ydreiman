55c125fb94673c6224767ffa1f9b8c39b76ba7d9
Yanis Dreymanis <ydreiman@gmail.com>
2021-05-29 00:56:35 +0300

    Переведите текст вида border-left-width в borderLeftWidth

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+  if (str === '') {
+    return str;
+  }
+
+  let arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i - 1] === '-') {
+      arr[i] = arr[i].toUpperCase();
+    }
+  }
+  return arr.filter(x => x !== '-').join('');
 }
