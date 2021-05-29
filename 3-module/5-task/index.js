4c889f5458e712d69d57c079afde326d8a63ca48
Yanis Dreymanis <ydreiman@gmail.com>
2021-05-29 18:34:50 +0300

    Найти min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  let arrOfNumbers = str
+    .replace(/,/g, ' ')
+    .split(' ')
+    .map(x => +x)
+    .filter(x => !isNaN(x));
+
+  return {
+    min: Math.min(...arrOfNumbers),
+    max: Math.max(...arrOfNumbers),
+  }
 }
