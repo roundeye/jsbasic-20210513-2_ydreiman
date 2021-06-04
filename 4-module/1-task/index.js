ced3a4ed7bbb79ba2a823471523eae0f0464a570
Yanis Dreymanis <ydreiman@gmail.com>
2021-06-05 02:15:03 +0300

    Занятие 4: задача 2

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -8,7 +8,7 @@ function makeFriendsList(friends) {
   li_1.textContent = `${friends[0].firstName} ${friends[0].lastName}`;
   li_2.textContent = `${friends[1].firstName} ${friends[1].lastName}`;
   li_3.textContent = `${friends[2].firstName} ${friends[2].lastName}`;
-
+  
   body.append(ul);  
   ul.append(li_1, li_2, li_3);
   return ul
--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  return table.rows[0].cells[0].style.backgroundColor = 'red',
+  table.rows[1].cells[1].style.backgroundColor = 'red',
+  table.rows[2].cells[2].style.backgroundColor = 'red',
+  table.rows[3].cells[3].style.backgroundColor = 'red',
+  table.rows[4].cells[4].style.backgroundColor = 'red'
 }
