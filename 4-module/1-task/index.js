c8007ce4a282b373dc7682a8271911e9b316a1af
Yanis Dreymanis <ydreiman@gmail.com>
2021-06-04 23:51:29 +0300

    Задание 3: задача 4 - правка, Задание 4: решение 1 задачи

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,15 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let body = document.body;
+  let ul = document.createElement('ul');
+  let li_1 = document.createElement('li')
+  let li_2 = document.createElement('li')
+  let li_3 = document.createElement('li')
+
+  li_1.textContent = `${friends[0].firstName} ${friends[0].lastName}`;
+  li_2.textContent = `${friends[1].firstName} ${friends[1].lastName}`;
+  li_3.textContent = `${friends[2].firstName} ${friends[2].lastName}`;
+
+  body.append(ul);  
+  ul.append(li_1, li_2, li_3);
+  return ul
 }
