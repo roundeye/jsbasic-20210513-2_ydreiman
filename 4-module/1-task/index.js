c06b4db3ced18edcc5dfe554d5a9526fe1806606
Yanis Dreymanis <ydreiman@gmail.com>
2021-06-08 12:49:32 +0300

    Занятие 4: правка 1 задачи(убрал лишнее)

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,13 +1,10 @@
 function makeFriendsList(friends) {
-  let body = document.body;
   let ul = document.createElement('ul');
-  body.append(ul); 
 
   for (let i = 0; i < friends.length; i++) {
     let li = document.createElement('li');
     li.textContent = `${friends[i].firstName} ${friends[i].lastName}`;
     ul.append(li);
   }
-  
   return ul
 }
