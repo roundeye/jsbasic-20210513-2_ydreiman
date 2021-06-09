322398ee864478a74c092b4d1b09551cda938a5a
Yanis Dreymanis <ydreiman@gmail.com>
2021-06-09 23:34:36 +0300

    Занятие 5: 2 задача

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let div = document.querySelector('#text');
+  button.onclick = function() {
+    // div.hidden = !div.hidden;
+    div.toggleAttribute('hidden');
+  }
 }
