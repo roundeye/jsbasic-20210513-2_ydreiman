f0784d4d8ba2ae9346b71351750a2a3163c301cf
Yanis Dreymanis <ydreiman@gmail.com>
2021-06-13 19:43:18 +0300

    Занятие 5: 3 задача

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,31 @@
 function initCarousel() {
-  // ваш код...
+  let carouselArrowRight = document.querySelector('.carousel__arrow_right');
+  let carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+  let carouselInner = document.querySelector('.carousel__inner');
+  let slideWidth = document.querySelector('.carousel__slide').offsetWidth;
+  let currentSlide = 1;
+  carouselArrowLeft.style.display = 'none';
+
+  function right() {
+    currentSlide++;
+    carouselInner.style.transform = `translateX(-${slideWidth * (currentSlide - 1)}px)`;
+    if (currentSlide === 4) {
+      carouselArrowRight.style.display = 'none';
+    } else {
+      carouselArrowLeft.style.display = '';
+    }
+  }
+
+  function left() {
+    currentSlide--;
+    carouselInner.style.transform = `translateX(-${slideWidth * (currentSlide - 1)}px)`;
+    if (currentSlide === 1) {
+      carouselArrowLeft.style.display = 'none';
+    } else {
+      carouselArrowRight.style.display = '';
+    }
+  }
+
+  carouselArrowRight.addEventListener('click', right);
+  carouselArrowLeft.addEventListener('click', left);
 }
