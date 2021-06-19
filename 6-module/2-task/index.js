06a8b1c6ef40c2097231f71928d31c33ea24d5fd
Yanis Dreymanis <ydreiman@gmail.com>
2021-06-19 17:15:30 +0300

    Занятие 6: задача 2

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -2,5 +2,29 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
   constructor(product) {
+    this.elem = document.createElement('div');
+    this.elem.classList.add('card');
+    this.render(product);
+    this.elem.closest('.card__button').addEventListener('click', (event) => this.onClick(event));
+  }
+
+  render(product) {
+    this.elem.innerHTML = `
+      <div class="card__top">
+          <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+          <span class="card__price">€${product.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+          <div class="card__title">${product.name}</div>
+          <button type="button" class="card__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+      </div>
+    `;
+  }
+
+  onClick(event) {
+    let event = new CustomEvent('product-add', {bubbles: true, detail: product.id});
+    this.elem.dispatchEvent(event);
   }
 }
