577fb1e8849b14c47fd997de2ad664f9a84098c2
Yanis Dreymanis <ydreiman@gmail.com>
2021-12-19 01:00:12 +0300

    1-module: 1 task (изменил цикл)

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,12 +1,26 @@
+// function factorial(n) {
+//   let result = 1;
+
+//   if (n === 0 || n === 1) {
+//     return result;
+//   } else {
+//     for (let i = 2; i <= n; i++) {
+//       result *= i;
+//     }
+//     return result;
+//   }
+// }
+
 function factorial(n) {
   let result = 1;
 
-  if (n === 0 || n === 1) {
-    return result;
+  if (n === 0) {
+      return result;
   } else {
-    for (let i = 2; i <= n; i++) {
-      result *= i;
-    }
-    return result;
+      for (let i = 1; i <= n; i++) {
+          result *= i;
+      }
   }
+
+  return result;
 }
